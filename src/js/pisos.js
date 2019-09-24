4ac0bc7444f585ff1c0c6b5f11f58ec3d2fbd881
Иван Питенин <ivan.pitenin@gmail.com>
2019-09-24 17:11:36 +0300

    add webpack and some fix

---
+++ src/js/pisos.js
@@ -0,0 +1,7 @@
+document.addEventListener('click', function() {
+	if (document.querySelector('header').style.backgroundColor === 'rgb(64, 224, 208)') {
+		document.querySelector('header').style.backgroundColor = '#ffc0cb';
+	} else {
+		document.querySelector('header').style.backgroundColor = 'rgb(64, 224, 208)';
+	}
+})
